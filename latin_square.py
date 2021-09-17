181eabdd50208c57bd25203973a6a1e472eec0a9
William Ehlhardt <williamehlhardt@gmail.com>
2021-09-17 12:47:33 +0000

    check roman citizenship

--- latin_square.py
+++ latin_square.py
@@ -15,6 +15,12 @@ SPACE = range(N)
 def m(a,b) -> int:
     return SQUARE[a][b]
 
+def is_latin(square):
+    return (
+        all(len(set(row)) == len(row) for row in square) and
+        all(len(set(col)) == len(col) for col in zip(*square))
+        )
+
 def associative_counterexample(m, space):
     for a,b,c in itertools.product(space, repeat=3):
          if m(a, m(b, c)) != m(m(a,b), c):
@@ -25,14 +31,15 @@ def commutative_counterexample(m, space):
         if m(a,b) != m(b,a):
             return (a,b)
 
+assert is_latin(SQUARE)
+assert associative_counterexample(m, SPACE) is None
+assert commutative_counterexample(m, SPACE) is None
+
 def solve(m,space,view,x):
     q = functools.reduce(m, view)
     for c in space:
         if m(q,c) == x:
             return c
-
-print(associative_counterexample(m, SPACE))
-print(commutative_counterexample(m, SPACE))
 
 def pl(*args, **kwargs):
     print(*args, end=' ', **kwargs)
