f033d582e2f255f8b18028516d155bcc79152747
William Ehlhardt <williamehlhardt@gmail.com>
2021-09-22 20:02:48 -0700

    implement initial hypercube!

---
+++ hypercube.py
@@ -0,0 +1,81 @@
+import itertools
+
+N=3
+SPACE = list(range(N))
+CUBE = [
+    [
+        [0, 1, 2],
+        [0, 2, 2],
+        [1, 0, 1],
+    ],
+    [
+        [1, 2, 0],
+        [0, 1, 1],
+        [2, 0, 2],
+    ],
+    [
+        [2, 0, 1],
+        [2, 1, 0],
+        [1, 2, 0],
+    ],
+]
+
+def solve(player, view_left, view_right):
+    if player == 0:
+        assert view_left == []
+        row, layer = view_right
+        for column in range(3):
+            if CUBE[layer][row][column] == player:
+                return column
+        raise Exception("Not found!")
+    
+    if player == 1:
+        column, = view_left
+        layer, = view_right
+        for row in range(3):
+            if CUBE[layer][row][column] == player:
+                return row
+        raise Exception("Not found!")
+    
+    if player == 2:
+        column, row = view_left
+        assert view_right == []
+        for layer in range(3):
+            if CUBE[layer][row][column] == player:
+                return layer
+        raise Exception("Not found!")
+    
+    raise Exception("Invalid player!")
+
+def pl(*args, **kwargs):
+    print(*args, end=' ', **kwargs)
+
+def check():
+    all_solved = True
+    for gamestate in itertools.product(SPACE, repeat=N):
+        gamestate = list(gamestate) # thanks python
+        pl(gamestate)
+
+        solved=False
+        for player in range(N):
+            hidden = gamestate[player]
+            view_left =  gamestate[:player]
+            view_right = gamestate[player+1:]
+            guess = solve(player, view_left, view_right)
+            pl(f'{player}:{guess}')
+            if guess == hidden:
+                pl('✅')
+                solved=True
+            else:
+                pl('❌')
+
+        all_solved = all_solved and solved
+        if not solved:
+            pl('NOT SOLVED')
+
+        print()
+    if not all_solved:
+        print('❌❌❌❌❌❌❌ FAILED TO SOLVE ALL ❌❌❌❌❌❌❌')
+    return all_solved
+
+check()
